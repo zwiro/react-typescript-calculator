faaa660cc06b4576cf8f9fa7e5f9a2a453047149
zwiro <josiu100@gmail.com>
2023-02-23 17:25:31 +0100

    Digits order fixed

--- src/components/DigitButton.tsx
+++ src/components/DigitButton.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import { ACTIONS } from "../App"
 
 type DigitButtonProps = {
@@ -8,11 +9,29 @@ type DigitButtonProps = {
 }
 
 function DigitButton({ digit, dispatch }: DigitButtonProps) {
+  const [digitRow, setDigitRow] = useState("")
+
+  useEffect(() => {
+    switch (digit) {
+      case "1":
+      case "2":
+      case "3":
+        return setDigitRow("2")
+      case "4":
+      case "5":
+      case "6":
+        return setDigitRow("3")
+      case "7":
+      case "8":
+      case "9":
+        return setDigitRow("4")
+    }
+  }, [])
+
   return (
     <button
       onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
-      className={`bg-zinc-500 p-6 font-bold transition-all hover:brightness-75 active:shadow-inner active:shadow-black 
-      ${""}`}
+      className={`bg-zinc-500 p-6 font-bold transition-all hover:brightness-75 active:shadow-inner active:shadow-black row-start-${digitRow}`}
     >
       {digit}
     </button>
