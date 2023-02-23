a915caaa5b04c094be249fb5596d50a8f0ed7206
zwiro <josiu100@gmail.com>
2023-02-23 16:24:18 +0100

    Moved Buttons to components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import Button from "./Button"
+import Button from "./components/Button"
 
 function App() {
   const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "0"]
   return (
     <main className="grid h-screen place-items-center">
       <div className="w-1/2">
-        <div className="w-full bg-zinc-900 p-4 text-right">
+        <div className="h-28 w-full bg-zinc-900 px-4 py-8 text-right">
           <p>
-            5322 <span>*</span>
+            <span></span>
           </p>
-          <p className="text-2xl font-bold">5555</p>
+          <p className="text-2xl font-bold"></p>
         </div>
         <div className="grid w-full grid-cols-4 grid-rows-5 gap-px">
           <Button span={2}>AC</Button>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,20 @@
 type ButtonProps = {
   children: React.ReactNode
-  span: number
-  index: number
+  span?: number
+  index?: number
 }
 
 function Button({ children, span, index }: ButtonProps) {
   return (
     <button
-      className={`bg-zinc-500 p-4 font-bold transition-all hover:brightness-75 col-span-${span} ${
-        index < 3
+      className={`bg-zinc-500 p-6 font-bold transition-all hover:brightness-75 active:shadow-inner active:shadow-black col-span-${span} 
+      ${
+        index &&
+        (index < 3
           ? "row-start-2"
           : index < 6
           ? "row-start-3"
-          : index < 9 && "row-start-4"
+          : index < 9 && "row-start-4")
       }`}
     >
       {children}
